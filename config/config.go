eb939e162fb386699a70d846ce96462ff8d418d2
zhangkesheng <clark.zhang@mingyizhudao.com>
2017-11-10 18:06:58 +0800

    fetch update init env

--- config/config.go
+++ config/config.go
@@ -1,5 +1,7 @@
 package config
 
+import "os"
+
 type CommonConf struct {
 	ConsulInternal  string
 	WeaveHostCidr   string
@@ -19,6 +21,18 @@ func getDefaultConfig() *CommonConf {
 }
 
 func LoadCommonConf() *CommonConf {
-	//todo
-	return getDefaultConfig()
+	conf := getDefaultConfig()
+	if os.Getenv("CONSUL_INTERNAL") != "" {
+		conf.ConsulInternal = os.Getenv("CONSUL_INTERNAL")
+	}
+	if os.Getenv("WEAVE_HOST_CIDR") != "" {
+		conf.WeaveHostCidr = os.Getenv("WEAVE_HOST_CIDR")
+	}
+	if os.Getenv("CONSUL_HTTP_ADDR") != "" {
+		conf.ConsulHttpAddr = os.Getenv("CONSUL_HTTP_ADDR")
+	}
+	if os.Getenv("IGNORE_CONTAINER") != "" {
+		conf.IgnoreContainer = os.Getenv("IGNORE_CONTAINER")
+	}
+	return conf
 }
